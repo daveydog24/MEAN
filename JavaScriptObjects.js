9effbf76deb152589a2ee1c7b8c0919e5d233f13
David Wukelic <36245241+daveydog24@users.noreply.github.com>
2018-06-11 19:57:14 -0700

    Add files via upload

--- JavaScriptObjects.js
+++ JavaScriptObjects.js
@@ -1,15 +1,21 @@
-// let students = [
-//     {name: 'Remy', cohort: 'Jan'},
-//     {name: 'Genevieve', cohort: 'March'},
-//     {name: 'Chuck', cohort: 'Jan'},
-//     {name: 'Osmund', cohort: 'June'},
-//     {name: 'Nikki', cohort: 'June'},
-//     {name: 'Boris', cohort: 'June'}
-// ];
+// part 1
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
 
-// for (var element of students){
-//     console.log("Name: " + element.name + ", Cohort: " + element.cohort);
-// }
+console.log("Part 1:")
+console.log("")
+for (var element of students){
+    console.log("Name: " + element.name + ", Cohort: " + element.cohort);
+}
+console.log("")
+
+// part 2
 let users = {
     employees: [
         {'first_name': 'Miguel', 'last_name': 'Jones'},
@@ -23,6 +29,9 @@ let users = {
     ]
 };
 
+console.log("Part 2:")
+console.log("")
+
 console.log("EMPLOYEES")
 for (let [index, element] of users.employees.entries()){
     let length = ((element.last_name + element.first_name).length)
@@ -34,3 +43,17 @@ for (let [index, element] of users.managers.entries()){
     let length = ((element.last_name + element.first_name).length)
     console.log(index + 1 + " - " + element.last_name + ", " + element.first_name + " - " + length)
 }
+
+// after solutions i worked through it for the cleaner version:
+// 
+// for (const key in users) {
+//     console.log(key.toUpperCase());
+//     for (let index = 0; index < users[key].length; index++){
+//         const count = index + 1;
+//         const first_name = users[key][index].first_name;
+//         const last_name = users[key][index].last_name;
+//         const length = first_name.length + last_name.length;
+//         console.log(`${count} - ${last_name}, ${first_name} - ${length}`);
+//         }
+//         console.log("");
+// }
